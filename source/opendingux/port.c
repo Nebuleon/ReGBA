7b55a50eea79105f508ada053d0470f8c97b6f60
Nebuleon <nebuleon@alakazam>
2013-10-19 22:35:42 +0000

    OpenDingux: Fix self-overlapping FPS counter caused by commit 9d0e7a5405882c9ce982b5b0e79cb2f45612ad8f.

--- source/opendingux/port.c
+++ source/opendingux/port.c
@@ -111,6 +111,7 @@ void ReGBA_DisplayFPS(void)
 		char line[512];
 		sprintf(line, "%2u/%3u", Stats.RenderedFPS, Stats.EmulatedFPS);
 		// White text, black outline
+		ScaleModeUnapplied();
 		print_string_outline(line, RGB888_TO_RGB565(255, 255, 255), RGB888_TO_RGB565(0, 0, 0), 7, GCW0_SCREEN_HEIGHT - 19);
 	}
 }
